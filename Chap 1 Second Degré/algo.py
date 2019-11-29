a8098820bee5b6733030945ef626e623a98398e7
Pierre <pierremaurer@orange.fr>
2019-11-29 14:50:02 +0100

    first commit

---
+++ Chap 1 Second Degré/algo.py
@@ -0,0 +1,17 @@
+﻿a= float(input("Donné a"))
+b= float(input("Donné b"))
+c= float(input("Donné c"))
+
+delta=b*b-4*a*c
+print(delta)
+
+if delta < 0:
+    print("Aucune solutions disponibles")
+if delta==0:
+    resultat3=(-b/(2*a))
+    print("Le resulat est", resultat3)
+if delta > 0:
+    resultat=(-b-(delta/delta)/(2*a))
+    resultat1=(-b+(delta/delta)/(2*a))
+    print("Le premier résultat est",resultat)
+    print("Le deuxieme résultat est",resultat1)
---
+++ Chap 2 Suites numériques/86p33.py
@@ -0,0 +1,6 @@
+﻿p=iny(input("Donner le terme d'indice de la suite"))
+i = 1
+while i<p:
+    a=(5*i)/(1+i)**2
+    i=i+1
+print(a)
